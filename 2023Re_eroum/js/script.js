95b115a6f4444da30f875e7c24046703256d6484
jhkl0514 <jhkl0514@naver.com>
2023-02-10 16:23:50 +0900

    0210-05

--- 2023Re_eroum/js/script.js
+++ 2023Re_eroum/js/script.js
@@ -1,37 +1,7 @@
 console.log("test");
 
-// 메뉴
-        // $(".bg").slideUp()
-        // $(".navbar").hover(function(){
-        //     $(".bg,.submenu").stop().slideDown()
-        // },function(){
-        //     $(".bg,.submenu").stop().slideUp()
-        // })
-
-// 토글
-// $(".toggle").click(function(){
-//     $(".navbarClone").css("left",0);
-//     $(".toggle").addClass("active")
-//     $(".overlay").show();
-// })
-// $(".overlay").click(function(){
-//     $(".navbarClone").css("left","-100%");
-//     $(".toggle").removeClass("active")
-//     $(".overlay").hide();
-
-// })
-
-// 토글
-// $(".toggle").click(function(){
-//     $(".toggle").addClass("active")
-
-// })
-// $(".overlay").click(function(){
-//         $(".toggle").removeClass("active")
-// })
 
 let view = true;
-
 $(".toggle").click(function(){
     if( view == true){
         $(".toggle").addClass("active");
@@ -55,17 +25,19 @@ $(".main_menu li a").click(function(e){
 
 })
 
-// $(".navbarClone .mMenu>ul>li>a").click(function(e){
-//     e.preventDefault();
-//    $(".mMenu>ul>li>a").parent().find(".submenu").slideUp(200);
 
-//    if($(this).hasClass("active")){
-//      $(".mMenu>ul>li>a").removeClass("active")
-//    }else{
-//      $(this).parent().find(".submenu").stop().slideDown(200);
-//      $(".mMenu>ul>li>a").removeClass("active");
-//      $(this).addClass("active").addClass("");    
-
-//    }
-  
-//  })
+// Initialize Swiper               
+var swiper = new Swiper(".eventSwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  })
+                    
