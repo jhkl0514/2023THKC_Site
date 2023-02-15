547e20e8f0039bee5f80055f86aab6dac292abe9
jhkl0514 <jhkl0514@naver.com>
2023-02-15 14:06:42 +0900

    0215_06

--- 2023Re_eroum/js/script.js
+++ 2023Re_eroum/js/script.js
@@ -10,14 +10,12 @@ let view = true;
 $(".toggle").click(function(){
     if( view == true){
         $(".toggle").addClass("active");
-        $(".asideClone").css("left",0);
-        $(".bg").css("left",0);
+        $(".asideClone").css("left",0);       
         $(".overlay").show();
-        view = false;
+        
     }else{
         $(".toggle").removeClass("active");
-        $(".asideClone").css("left","-100%");
-        $(".bg").css("left","-100%");
+        $(".asideClone").css("left","-100%");      
         $(".overlay").hide();
         view = true;
     }
@@ -29,16 +27,7 @@ $(".overlay").click(function(){
 
 })
 
-// 메뉴
-$(".main_menu li a").click(function(e){
-    e.preventDefault();
-    if($(this).hasClass("active")){
-        $(".main_menu li a").addClass("active");  
-    }else{
-        $(".main_menu li a").removeClass("active");
-         $(this).addClass("active").addClass("");   
-    }
-})
+
 // aside 메뉴를 가져와서 복제하고 appendTo 한다.
 $("#asideWrap aside").clone().appendTo(".mobileAside")
 
