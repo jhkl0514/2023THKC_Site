81482fb80efc70da83121d9979725146aece7b45
jhkl0514 <jhkl0514@naver.com>
2023-03-07 11:45:00 +0900

    0307_05

--- 2023Re_eroum/js/new_js/thkc_script.js
+++ 2023Re_eroum/js/new_js/thkc_script.js
@@ -127,7 +127,7 @@ var swiper = new Swiper(".partnerSwiper", {
 });
 
 // 아이디패스워드 Tap
-  $(".thkc_tableTapWrap .thkc_tableWrap").eq(1).show()
+  $(".thkc_tableTapWrap .thkc_tableWrap").eq(1).hide()
   $(".tabTitleWrap .tabTitle>div").click(function(){
   $(".tabTitleWrap .tabTitle>div").removeClass("active")
   $(this).addClass("active")
