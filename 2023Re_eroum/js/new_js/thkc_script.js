e6ef5457f5e2aaf9d632e6c04db60f5df6008b80
jhkl0514 <105696294+jhkl0514@users.noreply.github.com>
2023-08-28 11:39:35 +0900

    0828

--- 2023Re_eroum/js/new_js/thkc_script.js
+++ 2023Re_eroum/js/new_js/thkc_script.js
@@ -61,7 +61,7 @@ $(".banner_eroum").clone().appendTo(".m_banner_eroum")
 // });
 
 var swiper = new Swiper(".menuWrap02 .mySwiper_menu", {
-  slidesPerView: 3,
+  slidesPerView: 1,
       spaceBetween: 1,
       pagination: {
         el: ".swiper-pagination",
